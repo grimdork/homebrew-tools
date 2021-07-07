db08484d0aa46bf651e95307f53f44c0cd4329db
Urethramancer <github@grimdiork.net>
2021-07-07 14:28:53 +0200

    Brew formula update for sec version v0.8.0

--- sec.rb
+++ sec.rb
@@ -5,29 +5,29 @@
 class Sec < Formula
   desc "Store secrets in AWS Parameter Store."
   homepage "https://github.com/grimdork/sec"
-  version "0.7.6"
+  version "0.8.0"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/grimdork/sec/releases/download/v0.7.6/sec_0.7.6_Darwin_x86_64.zip"
-      sha256 "c898f9e58893b7ef946924a179b97cfcc7d1efbea16cdbcca2a465f762c9e24c"
+      url "https://github.com/grimdork/sec/releases/download/v0.8.0/sec_0.8.0_Darwin_x86_64.zip"
+      sha256 "5f882adf213cb31b13edc8c9281ad8c26712d97f4e2422bac3fe85f013f7bf43"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/grimdork/sec/releases/download/v0.7.6/sec_0.7.6_Darwin_arm64.zip"
-      sha256 "e3af61c70d0d4b4aa1b7ee124d9c4386b277c45f11012924c57c2d590e30a14e"
+      url "https://github.com/grimdork/sec/releases/download/v0.8.0/sec_0.8.0_Darwin_arm64.zip"
+      sha256 "3800d57034984ab4bf1f90513dfa080eb5b6520df7b7bcf68bb7876f2bec396c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/grimdork/sec/releases/download/v0.7.6/sec_0.7.6_Linux_x86_64.tar.gz"
-      sha256 "5aa649575fdd467684e820f3ffca90f4413fced00e75e540a57af31ed4487414"
+      url "https://github.com/grimdork/sec/releases/download/v0.8.0/sec_0.8.0_Linux_x86_64.tar.gz"
+      sha256 "4d664d21c937b385bde2066c890415495b612f1c2e55629bdc460c4a9bc5e693"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/grimdork/sec/releases/download/v0.7.6/sec_0.7.6_Linux_arm64.tar.gz"
-      sha256 "17871483a5526a26917496c6b0de35dc99fa2b902ac842c3f9b669e1b7edb0bc"
+      url "https://github.com/grimdork/sec/releases/download/v0.8.0/sec_0.8.0_Linux_arm64.tar.gz"
+      sha256 "742a9d2ee29c82a9b43e5affd1afddba4ed04d92e00cd833abf354d34dce99ff"
     end
   end
 
